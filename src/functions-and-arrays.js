99b7123273204d41011dc6fa6d0cbe3f709ce416
Emilia Junghanns <emijunghanns@hotmail.com>
2024-10-24 16:22:01 +0200

    done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,15 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1,num2) {
+    if (num1 > num2){
+        return num1
+    } else if (num2 > num1) {
+        return num2
+    } else {
+        return num1
+    } 
+}   
+    // return Math.max(num1,num2)
+
 
 
 
@@ -7,7 +17,24 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    let biggestWord = ''
+    let temp = 0
+    if (arr.length === 0) {
+        return null
+    } 
+
+    for (let i = 0; i < arr.length; i++){
+       let letterNum = arr[i].length
+       if (letterNum>temp){
+        temp = letterNum
+        biggest = arr[i]
+       
+       }
+    }
+  return biggest
+}
+
 
 
 
@@ -15,7 +42,19 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr1) {
+    if (arr1.length === 0){
+        return 0
+    } else if (arr1.length === 1){
+        return arr1[0]
+    } else {
+        let sumNum = 0
+        for (let i=0; i<arr1.length; i++) {
+            sumNum += arr1[i]  
+        }
+      return sumNum  
+    }
+}
 
 
 
@@ -23,7 +62,18 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr2) {
+    if (arr2.length === 0){
+        return 0
+    } 
+    else {
+        let sumNum1 = 0
+        for (let i=0; i<arr2.length; i++){
+            sumNum1 += arr2[i]
+        }
+        return sumNum1/arr2.length
+    }
+}
 
 
 
@@ -31,4 +81,10 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr3, toFind) {
+    if (arr3.length === 0 ){
+        return null
+    } else {
+        return arr3.includes(toFind)
+    }
+}
